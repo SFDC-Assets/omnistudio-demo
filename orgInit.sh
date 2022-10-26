156a327232c55733d31b50cc31d0690d487b549d
zhou059 <zhou059@gmail.com>
2022-10-26 10:58:15 -0700

    update timeouts

--- orgInit.sh
+++ orgInit.sh
@@ -6,7 +6,8 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --us
 #install OmniStudio - 238
 sfdx force:package:install --package=04t4W000002keqb --wait 15 -r
 
-sfdx force:package:install --package=04t1E000000jb9R --wait 15
+4t1E000001IrkoQAC
+sfdx force:package:install --package=4t1E000001IrkoQAC --wait 15
 sfdx force:package:install --package=04t1E000001Iql5 --wait 2
 sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:user:permset:assign -n FinancialServicesCloudStandard
