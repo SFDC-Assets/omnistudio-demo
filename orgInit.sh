5c56412e5126eacaeeee63cf941af356cbb0464b
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2023-02-08 11:05:30 -0800

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -6,8 +6,8 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --us
 #install OmniStudio - 238
 sfdx force:package:install --package=04t4W000002keqb --wait 20 -r
 
-# sfdx force:package:install --package=04t1E00000127We --wait 20
-# sfdx force:package:install --package=04t1E000001Iql5 --wait 2
+sfdx force:package:install --package=04t1E00000127We --wait 20
+sfdx force:package:install --package=04t1E000001Iql5 --wait 2
 sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:user:permset:assign -n FinancialServicesCloudStandard
 sfdx force:user:permset:assign -n FinancialServicesCloudExtension
