37a12564d571c80dcd06ef439df87912f30fa1d6
zhou059 <zhou059@gmail.com>
2023-12-14 07:18:03 -0800

    updated package ids

--- orgInit.sh
+++ orgInit.sh
@@ -3,27 +3,27 @@
 #create scratch org
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix omni -o studio.demo
 
-#install OmniStudio - 238
-sfdx force:package:install --package=04t4W000002keqb --wait 20 -r
-
-# sfdx force:package:install --package=04t1E00000127We --wait 20
-# sfdx force:package:install --package=04t1E000001Iql5 --wait 2
+#install OmniStudio - 246.14
+sfdx force:package:install --package=04t4W0000038bMv --wait 20 -r
+# 04tHn000001dvY4
+sfdx force:package:install --package=04tHn000001dvY4 --wait 30
+sfdx force:package:install --package=04t1E000001Iql5 --wait 2
 sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:user:permset:assign -n FinancialServicesCloudStandard
 sfdx force:user:permset:assign -n FinancialServicesCloudExtension
 
 #push the utility metadata service class
-sfdx force:source:deploy -m ApexClass:MetadataService
+# sfdx force:source:deploy -m ApexClass:MetadataService
 
 #set the remote sites
-sfdx force:apex:execute -f dparemoteSites.cls
+# sfdx force:apex:execute -f dparemoteSites.cls
 
 #Puppeteer to set OmniStudio Metadata to TRUE via UI  and enable the  standard runtime
-node run.js
+# node run.js
 
 #Sample metadata examples into the org - basic hello world.
-sfdx force:source:deploy -p force-app
 sfdx force:source:deploy -p omnideploy
+sfdx force:source:deploy -p force-app
 
 #open says me.
 sfdx force:org:open
