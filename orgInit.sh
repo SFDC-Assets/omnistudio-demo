b2733fe6c4696277f2b2cfc2fffea862e8882ae3
Ryan Kellogg <rkellogg@rkellog-ltm2yhe.internal.salesforce.com>
2024-04-17 15:13:37 -0700

    some message

--- orgInit.sh
+++ orgInit.sh
@@ -4,7 +4,7 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix omni -o studio.demo
 
 #install OmniStudio - 246.14
-# Packages here: https://help.salesforce.com/s/articleView?id=000394906&type=1
+# Packages here: https://help.salesforce.com/s/articleView?id=000394906&type=1y
 sfdx force:package:install --package=04t4W0000038bmm --wait 20 -r
 sfdx force:package:install --package=04tHn000001dvY4 --wait 30
 sfdx force:package:install --package=04t1E000001Iql5 --wait 2
@@ -27,3 +27,4 @@ sfdx force:source:push -f
 
 #open says me.
 sfdx force:org:open
+
