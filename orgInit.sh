6715d68ec25230b4e550dc162889351b280e8c52
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-07-19 12:41:33 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -4,7 +4,7 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix omni -o studio.demo
 
 #install OmniStudio - 246.14
-# Packages here: https://help.salesforce.com/s/articleView?id=000394906&type=1y
+# Packages here: https://help.salesforce.com/s/articleView?id=sf.os_install_or_upgrade_the_omnistudio_package.htm&type=5
 sfdx force:package:install --package=04t4W000003ChfP --wait 20 -r
 sfdx force:package:install --package=04tHn000001dvY4 --wait 30
 sfdx force:package:install --package=04t1E000001Iql5 --wait 2
