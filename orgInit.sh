4583d4e966de4a3739852571849680056d74df85
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-13 00:11:07 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
 #create scratch org
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix omni -o studio.demo
+sf demoutil org create scratch -f config/project-scratch-def.json -d 30 -s -p omni -e studio.demo
 
 #install OmniStudio - 246.14
 # Packages here: https://help.salesforce.com/s/articleView?id=sf.os_install_or_upgrade_the_omnistudio_package.htm&type=5
-sfdx force:package:install --package=04t4W000003ChfP --wait 20 -r
-sfdx force:package:install --package=04tHn000001dvY4 --wait 30
-sfdx force:package:install --package=04t1E000001Iql5 --wait 2
-sfdx shane:user:password:set -p salesforce1 -g User -l User
-sfdx force:user:permset:assign -n FinancialServicesCloudStandard
-sfdx force:user:permset:assign -n FinancialServicesCloudExtension
+sf package install -p 04t4W000003ChfP --wait 20 -r
+sf package install -p 04tHn000001dvY4 --wait 30
+sf package install -p 04t1E000001Iql5 --wait 2
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org assign permset -n FinancialServicesCloudStandard
+sf org assign permset -n FinancialServicesCloudExtension
 
 #Sample metadata examples into the org - basic hello world.
-sfdx force:source:deploy -p omnideploy
-sfdx force:source:push -f
+sf project deploy start -d omnideploy
+sf project deploy start -f
 
 #open says me.
-sfdx force:org:open
-
+sf org open
